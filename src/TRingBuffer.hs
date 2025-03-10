cb6b34141e19876e71406b738c303bc80a64fd59
Qqwy / Marten <w-m@wmcode.nl>
2025-03-10 03:13:52 +0100

    Cache readIdx on writing / writeIdx on reading,
    so most of the times reads no longer block writes and vice-versa

--- src/TRingBuffer.hs
+++ src/TRingBuffer.hs
@@ -49,10 +49,14 @@ import System.IO.Unsafe qualified
 -- - https://en.wikipedia.org/wiki/Circular_buffer
 -- - https://rigtorp.se/ringbuffer/
 data TRingBuffer a = TRingBuffer
-  { reader :: !(TVar Word)
-  , writer :: !(TVar Word)
+  { readerAndCachedWriter :: !(TVar Indexes)
+  , writerAndCachedReader :: !(TVar Indexes)
   , contents :: !(TArray Word a)
   }
+
+data Indexes = Indexes
+  {-# UNPACK #-} !Word  -- ^ An up-to-date value
+  {-# UNPACK #-} !Word -- ^A cached value, updated only when needed
 
 instance Show (TRingBuffer a) where
     show buf = "TRingBuffer {capacity = " <> show (capacity buf) <> ", ...}"
@@ -60,18 +64,22 @@ instance Show (TRingBuffer a) where
 -- | Create a new TRingBuffer with the given max `capacity`
 empty :: Word -> STM (TRingBuffer a)
 empty cap = do
-  reader <- TVar.newTVar 0
-  writer <- TVar.newTVar 0
+  let readIdx = 0
+  let writeIdx = 0
+  readerAndCachedWriter <- TVar.newTVar (Indexes readIdx writeIdx)
+  writerAndCachedReader <- TVar.newTVar (Indexes writeIdx readIdx)
   contents <- emptyContents cap
-  pure TRingBuffer{reader, writer, contents}
+  pure TRingBuffer{readerAndCachedWriter, writerAndCachedReader, contents}
 
 -- | Create a new TRingBuffer, directly in IO (outside of STM)
 emptyIO :: Word -> IO (TRingBuffer a)
 emptyIO cap = do
-  reader <- TVar.newTVarIO 0
-  writer <- TVar.newTVarIO 0
+  let readIdx = 0
+  let writeIdx = 0
+  readerAndCachedWriter <- TVar.newTVarIO (Indexes readIdx writeIdx)
+  writerAndCachedReader <- TVar.newTVarIO (Indexes writeIdx readIdx)
   contents <- emptyContents cap
-  pure TRingBuffer{reader, writer, contents}
+  pure TRingBuffer{readerAndCachedWriter, writerAndCachedReader, contents}
 
 emptyContents :: (MArray a1 e m, Ix i, Integral a2, Num i) => a2 -> m (a1 i e)
 emptyContents cap = 
@@ -80,7 +88,7 @@ emptyContents cap =
     -- This is necessary because if the buffer is completely full,
     -- we still want the write index to be one less than the read index.
     -- (Otherwise it would be indistinguishable from the 'buffer completely empty' case).
-    Array.newArray (0, fromIntegral (cap + 1)) emptyElem
+    Array.newArray (0, fromIntegral cap) emptyElem
 
 -- | Check the maximum number of elements that can be stored in this TRingBuffer
 --
@@ -92,9 +100,8 @@ capacity buf =
     -- we never grow or shrink it,
     -- so this call is outwardly pure
     let cap = System.IO.Unsafe.unsafePerformIO $ capacity' buf
-    in
-    -- See NOTE [capacity]
-        cap - 1
+    in -- See NOTE [capacity]
+      cap - 1
 
 -- Returns the _true_ capacity, including the extra element
 capacity' :: MArray TArray a m => TRingBuffer a -> m Word
@@ -108,19 +115,32 @@ capacity' buf = fromIntegral <$> Array.getNumElements buf.contents
 -- Non-blocking. A worst-case O(1) constant-time operation.
 --
 -- Calls to `tryPush` are synchronized with any other concurrent calls to
--- `pop`/`push`/`tryPop`/`tryPush` (using `STM.retry`)
+-- `push`/`tryPush` (using `STM.retry`)
 tryPush :: TRingBuffer a -> a -> STM Bool
 tryPush buf a = do
   !cap <- capacity' buf
-  readIdx <- TVar.readTVar buf.reader
-  writeIdx <- TVar.readTVar buf.writer
+  Indexes writeIdx readIdxCached <- TVar.readTVar buf.writerAndCachedReader
   let newWriteIdx = (writeIdx + 1) `mod` cap
-  if newWriteIdx == readIdx then
-      -- Buffer is full
+  if newWriteIdx == readIdxCached then do
+    -- Buffer may be full, double-check with real read index
+    Indexes readIdxFresh _ <- TVar.readTVar buf.readerAndCachedWriter
+    if newWriteIdx == readIdxFresh then
+      -- Buffer is truly full, nothing more to do
+      -- (If the caller calls `STM.retry` after this point, any `tryPop` will trigger a transaction re-try!)
       pure False
+    else do
+      -- Buffer not full after all.
+      -- Save value, update write idx 
+      -- and also update cached read idx
+      actuallyPush writeIdx newWriteIdx readIdxFresh
   else do
+      -- There is certainly space
+      -- Save value, update write idx
+      actuallyPush writeIdx newWriteIdx readIdxCached
+  where
+    actuallyPush writeIdx newWriteIdx readIdx = do
       Array.writeArray buf.contents writeIdx a
-      TVar.writeTVar buf.writer newWriteIdx
+      TVar.writeTVar buf.writerAndCachedReader (Indexes newWriteIdx readIdx)
       pure True
 
 -- | Attempts to grab the earliest-written element from the TRingBuffer.
@@ -129,23 +149,36 @@ tryPush buf a = do
 -- Non-blocking. A worst-case O(1) constant-time operation.
 --
 -- Calls to `tryPop` are synchronized with any other concurrent calls to
--- `pop`/`push`/`tryPop`/`tryPush` (using `STM.retry`)
+-- `pop`/`tryPop` (using `STM.retry`)
 tryPop :: TRingBuffer a -> STM (Maybe a)
 tryPop buf = do
-  !cap <- capacity' buf
-  readIdx <- TVar.readTVar buf.reader
-  writeIdx <- TVar.readTVar buf.writer
-  if readIdx == writeIdx then
-      -- Buffer is empty
-      pure Nothing
+  Indexes readIdx writeIdxCached <- TVar.readTVar buf.readerAndCachedWriter
+  if readIdx == writeIdxCached then do
+      -- Buffer may be empty, double-check with real write index
+      Indexes writeIdxFresh _ <- TVar.readTVar buf.writerAndCachedReader
+      if readIdx == writeIdxFresh then do
+        -- Buffer is truly empty, nothing more to do
+        -- (If the caller calls `STM.retry` after this point, any `tryPush` will trigger a transaction re-try!)
+        pure Nothing
+      else do
+        -- Buffer not empty after all.
+        -- Extract value, update read idx
+        -- and also update cached write idx
+        actuallyPop readIdx writeIdxFresh
   else do
+    -- Buffer definitely non-empty
+    -- Extract value, (only) update read idx
+    actuallyPop readIdx writeIdxCached
+  where
+    actuallyPop readIdx writeIdx = do
       a <- Array.readArray buf.contents readIdx
       -- NOTE: The next line is not 100% necessary,
       -- but without it, we would hold on to a reference to `a`
       -- until overwritten by a later write, potentially delaying it being GC'd
       Array.writeArray buf.contents readIdx emptyElem
+      !cap <- capacity' buf
       let newReadIdx = (readIdx + 1) `mod` cap
-      TVar.writeTVar buf.reader newReadIdx
+      TVar.writeTVar buf.readerAndCachedWriter (Indexes newReadIdx writeIdx)
       pure (Just a)
 
 -- | Adds a new element to the TRingBuffer.
@@ -157,8 +190,10 @@ tryPop buf = do
 -- `pop`/`push`/`tryPop`/`tryPush` (using `STM.retry`)
 push :: TRingBuffer a -> a -> STM ()
 push buf a = do
-    writingSucceeded <- tryPush buf a
-    STM.check writingSucceeded
+    res <- tryPush buf a
+    case res of
+        False -> STM.retry
+        True -> pure ()
 
 -- | Reads the oldest element from the TRingBuffer.
 --
